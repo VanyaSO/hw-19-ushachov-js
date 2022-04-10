68dde6c22005ab7a2f924ccbeb8ec96281c5569f
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-04-11 00:25:12 +0300

    Ответь на вопрос я его закоментил

--- js/script.js
+++ js/script.js
@@ -1,21 +1,61 @@
 'use strict'
 
-const number = +prompt('Ведите число');
-let isSimple = true;
+// !!!!!!!!!!!!!!!!!!!
+// По уловию в дз запросить число должно находиться в функции
+// не знаю правильно ли я сделал что каждый раз вызываю новую цункцию и оно каждый раз просит ввести число
+// Но если запросить число не в функции и записать в глобальные переменые (вроде так оно называеться)
+// то оно потом просто выведет по очереди ответи
+// я может не правильно сделал
+// я закоментил второй вариант как могло быть
 
-if(number === 1) {
-    console.log(false)
-} else if(number > 1) {
+// если я сделаю эти переменые не в функции будет ошибка ?
+//надеюсь понял вопрос ))))))
 
+//разкоментить
+// let number = +prompt('Введите число', 1);
+// let degree = +prompt('Введите степень', 1);
 
-    for (let i = 2; i < number; i++){
-        if(number % i === 0){
-            isSimple = false;
-            break;
-        }
-    }
-    console.log(isSimple)
+function mainFunction(callback){
 
+    //закоментить
+    //закрить открить страницу заново ;)
+    let number = +prompt('Введите число', 1);
+    let degree = +prompt('Введите степень', 1);
+
+    callback(number, degree);
 }
 
+function exponentiation(num, deg){
+
+    let answerExp = num ** deg;
+
+    return alert(`${answerExp}`);
+}
+
+//Доп function
+function multiplay(num, deg){
+
+    let answerMul = num * deg;
+
+    return alert(`${answerMul}`);
+}
+
+function division(num, deg){
+
+    let answerDiv = num / deg;
+
+    return alert(`${answerDiv}`);
+}
+
+function modulo(num, deg){
+
+    let answerMod = num * deg;
+
+    return alert(`${answerMod}`);
+}
+
+mainFunction(exponentiation);
+mainFunction(multiplay);
+mainFunction(division);
+mainFunction(modulo);
 
